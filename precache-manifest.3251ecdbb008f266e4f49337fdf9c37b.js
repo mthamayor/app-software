9162d164b034271c2f84d619289eb798f3c707d5
Anifowose Habeeb <anifowosehabeeb@gmail.com>
2019-07-24 17:34:55 +0100

    Updates

---
+++ precache-manifest.3251ecdbb008f266e4f49337fdf9c37b.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7dbce6cbbd841b5035d98a5d1d8f6f0",
+    "url": "/app-software/index.html"
+  },
+  {
+    "revision": "59dcdafda088e51fdde8",
+    "url": "/app-software/static/css/main.c7f7f50e.chunk.css"
+  },
+  {
+    "revision": "785b4ac742e6e8fdc0cf",
+    "url": "/app-software/static/js/2.f9330896.chunk.js"
+  },
+  {
+    "revision": "59dcdafda088e51fdde8",
+    "url": "/app-software/static/js/main.f2a09c99.chunk.js"
+  },
+  {
+    "revision": "8c5c99721d8f83ec6900",
+    "url": "/app-software/static/js/runtime~main.307e6923.js"
+  },
+  {
+    "revision": "c8da0eb1b4e34da42307d3c5bf4e4ffb",
+    "url": "/app-software/static/media/analogous.c8da0eb1.png"
+  },
+  {
+    "revision": "76f040d2d499cf30e6631353848246db",
+    "url": "/app-software/static/media/livestock-farm.76f040d2.jpg"
+  },
+  {
+    "revision": "9dcf344b444fa64bcffe2706a44d00a5",
+    "url": "/app-software/static/media/open-drainage.9dcf344b.jpg"
+  },
+  {
+    "revision": "432ecfb28fc3639f41e24327f1f4546b",
+    "url": "/app-software/static/media/percolating-filter.432ecfb2.jpg"
+  },
+  {
+    "revision": "4a1a27afe60f15820aa65ea685e003cc",
+    "url": "/app-software/static/media/ph-scale.4a1a27af.jpg"
+  },
+  {
+    "revision": "88a87fb0acffbf46af1211778a69e053",
+    "url": "/app-software/static/media/plant-analogous.88a87fb0.png"
+  },
+  {
+    "revision": "bdb81c1b9b4cecdefebec9e5b8e5c7f0",
+    "url": "/app-software/static/media/polluted-water.bdb81c1b.jpg"
+  },
+  {
+    "revision": "82ab2939603821764524bda96925c35c",
+    "url": "/app-software/static/media/rain-pollutant.82ab2939.jpg"
+  },
+  {
+    "revision": "3b43136eb3516f9f53dc42abf38ea1f5",
+    "url": "/app-software/static/media/screen-chamber.3b43136e.jpg"
+  },
+  {
+    "revision": "bc9eae93674a88e93ae5598a99b719b9",
+    "url": "/app-software/static/media/sewage-treatment-methods.bc9eae93.jpg"
+  },
+  {
+    "revision": "5a34d7679ed495915e482627fc740f4e",
+    "url": "/app-software/static/media/sewage-treatment-system.5a34d767.jpg"
+  },
+  {
+    "revision": "a0a8de7d90f0b7774a30679789a39aad",
+    "url": "/app-software/static/media/wastewater-pond-system.a0a8de7d.jpg"
+  }
+]);
